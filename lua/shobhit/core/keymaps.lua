10ec21a26af843318793ff97ba8722533d07a91c
slyeet03 <slyeet03@gmail.com>
2024-06-13 12:25:58 +0530

    added rust support

--- lua/shobhit/core/keymaps.lua
+++ lua/shobhit/core/keymaps.lua
@@ -11,7 +11,6 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
-
 -- increment/decrement numbers
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
@@ -20,6 +19,7 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decremen
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sq", "<C-w>w", { desc = "Jump between split windows" }) -- jump between split windows
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
---
+++ lua/shobhit/plugins/rustaceanvim.lua
@@ -0,0 +1,13 @@
+return {
+	"mrcjkb/rustaceanvim",
+	version = "^4", -- Recommended
+	lazy = false, -- This plugin is already lazy
+
+	config = function()
+		local bufnr = vim.api.nvim_get_current_buf()
+		vim.keymap.set("n", "<leader>a", function()
+			vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+			-- or vim.lsp.buf.codeAction() if you don't want grouping.
+		end, { silent = true, buffer = bufnr })
+	end,
+}
